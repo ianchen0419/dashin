233734eaa4fb3844663f85e206b4d58b80bd7048
YianChen <ianchen0419@gmail.com>
2021-03-22 23:10:03 +0800

    404

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header();?>
+
+<div id="visual">
+	<div class="page-title">
+		<img src="<?php echo get_stylesheet_directory_uri().'/img/title.png' ?>" height="100%" />
+		<div class="wrapper-size">
+			<h1 class="has-blue-color has-text-align-center">此頁面不存在</h1>
+		</div>
+	</div>
+</div>
+<main id="contact">
+	<p>
+		<a href="<?php $home_url ?>">回到首頁</a>
+	</p>
+</main>
+
+
+<?php get_footer();?>
